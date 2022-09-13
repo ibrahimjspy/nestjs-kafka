6d2293c03bf02d374657b5d9322d0eff1df083e0
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-09-13 17:34:14 +0500

    Implementation : Adding dynamic queries support .

--- src/graphql/queries/updateProduct.ts
+++ src/graphql/queries/updateProduct.ts
@@ -1,11 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { gql } from 'graphql-request';
 
 export const updateProductQuery = (productData) => {
+  const dynamic_product_queries = () => {
+    for (const [key, value] of Object.entries(productData)) {
+      return `${key}: ${value}`
+    }
+  };
   return gql`
     mutation{
   productUpdate(
     id:${productData.product_id}
     input: {
+      ${dynamic_product_queries()}
       price: ${productData.product_catalog_price}
       name: "${productData.name}"
       slug: "${productData.slug}"
